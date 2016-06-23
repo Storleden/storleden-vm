aeed7f588dc2fdc87cf3d38393a3b27795087b3e
Luigi Guevara <luigi.fox15@gmail.com>
2016-06-23 12:16:33 +0200

    Updating script bash to run composer without xdebug, increase ruby version

--- puphpet/files/startup-always-unprivileged/00-composer-setup.sh
+++ puphpet/files/startup-always-unprivileged/00-composer-setup.sh
@@ -4,6 +4,6 @@ echo "export PATH=\$PATH:\$HOME/.config/composer/vendor/bin" >> $HOME/.bashrc
 
 echo '' >> $HOME/.bashrc
 
-echo 'function composer() { COMPOSER="$(which composer)" || { echo "Could not find composer in path" >&2 ; return 1 ; } && sudo php5dismod -s cli xdebug ; $COMPOSER "$@" ; STATUS=$? ; sudo php5enmod -s cli xdebug ; return $STATUS ; }' >> $HOME/.bashrc
+echo 'function composer() { COMPOSER="$(which composer)" || { echo "Could not find composer in path" >&2 ; return 1 ; } && sudo phpdismod -s cli xdebug ; $COMPOSER "$@" ; STATUS=$? ; sudo phpenmod -s cli xdebug ; return $STATUS ; }' >> $HOME/.bashrc
 
 echo '' >> $HOME/.bashrc
